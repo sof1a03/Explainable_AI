e144e7a89adad6332eea58500f9577a969969240
Lou1108 <72265570+Lou1108@users.noreply.github.com>
2025-03-13 08:00:44 +0100

    initial version of assignment3 and test classes

---
+++ Project_2/assignment3.py
@@ -0,0 +1,169 @@
+from anytree import AnyNode
+import json
+from itertools import product
+from anytree import PreOrderIter
+
+with open("coffee.json", "r") as file:
+    json_tree = json.load(file)
+
+
+def build_tree(tree, parent=None):
+    data = {
+        key: (
+            [int(v) if isinstance(v, (int, float)) else v for v in value] if isinstance(value, list)
+            else int(value) if isinstance(value, (int, float))
+            else value
+        )
+        for key, value in tree.items()
+        if key != "children"
+    }
+
+    node = AnyNode(parent=parent, **data)
+
+    # process children
+    for child in tree.get("children", []):
+        build_tree(child, parent=node)
+
+    return node
+
+
+def execution_traces(goal_tree_json, starting_node_name):
+    """
+    determines all possible behaviors (execution traces) that an agent could exhibit, based on its goal tree
+    :param goal_tree_json: json file representing a goal tree
+    :param starting_node_name: the name of the root node/ starting node for the traversal
+    :return: list of lists representing execution traces starting from the root, with nodes being represented by their name
+    """
+    # convert json input to proper format
+    goal_tree = goal_tree_json
+
+    # get starting node in tree
+    start_node = goal_tree
+    for node in goal_tree.descendants:
+        if start_node.name == starting_node_name:
+            break
+        else:
+            start_node = node
+
+    # recursively traverse tree to generate traces
+    def _get_traces(node):
+        # leave nodes are action nodes
+        if node.type == "ACT":
+            return [[node.name]]
+
+        elif node.type == "OR":
+            traces = []
+            for child in node.children:
+                for trace in _get_traces(child):
+                    traces.append([node.name] + trace)
+
+            return traces
+
+        # list all in first visit order
+        elif node.type == "AND" or node.type == "SEQ":
+            parallel_traces = [_get_traces(child) for child in node.children]
+            all_trace_list = [list(comb) for comb in product(*parallel_traces)]
+
+            children_ordered = sorted(node.children, key=lambda x: x.sequence)
+            children_ranked = {child.name: i for i, child in enumerate(children_ordered)}
+            sorted_traces = [sorted(t, key=lambda lst: children_ranked.get(lst[0], float('inf'))) for t in
+                             all_trace_list]
+
+            current_traces = [[node.name] + sum(i, []) for i in sorted_traces]
+            return current_traces
+
+    return _get_traces(start_node)
+
+
+def get_normed_tree(tree, norm):
+    if len(norm) == 0:
+        return tree
+
+    def check_violation(node, norm):
+        if node.type == 'ACT':
+            action_name = node.name
+            if norm['type'] == 'P':  # Prohibition: Action should not be executed
+                node.violation = action_name in norm['actions']
+            elif norm['type'] == 'O':  # Obligation: Only permitted actions should be executed
+                node.violation = action_name not in norm['actions']
+            else:
+                node.violation = False  # Default: No violation
+
+        elif node.type in ['OR', 'AND', 'SEQ']:
+            for child in node.children:
+                check_violation(child, norm)
+
+            if node.type == 'OR':
+                node.violation = all(child.violation for child in node.children)
+            elif node.type == 'AND' or node.type == 'SEQ':
+                node.violation = any(child.violation for child in node.children)
+
+    check_violation(tree, norm)
+    return tree
+
+
+def get_all_valid_traces(json_tree, pre, post):
+    traces_with_pre_post = []
+    for node in PreOrderIter(json_tree):
+        node_traces = execution_traces(json_tree, node.name)
+        for trace in node_traces:
+            if all([not node.violation for node in PreOrderIter(json_tree) if
+                    hasattr(node, "pre") and node.name in trace]):
+                pre_nodes = [node.pre for node in PreOrderIter(json_tree) if
+                             hasattr(node, "pre") and node.name in trace]
+                pre_nodes_list = [item for pre_node in pre_nodes for item in pre_node]
+                if all([p in pre_nodes_list for p in pre]):
+                    post_nodes = [node.post for node in PreOrderIter(json_tree) if
+                                  hasattr(node, "post") and node.name in trace]
+                    post_nodes_list = [item for post_node in post_nodes for item in post_node]
+                    if all([p in post_nodes_list for p in post]):
+                        traces_with_pre_post.append(trace)
+
+    return traces_with_pre_post
+
+
+def decision_making(json_tree, norm, goal, beliefs, preferences):
+    """
+    :param json_tree: son object: A goal tree
+    :param norm: dict: norm for violations in the tree
+    :param goal: list: set of beliefs (strings) of the agent that must be true at the end of the execution of the trace
+    :param beliefs: list: set of strings representing the initial beliefs of the agents
+    :param preferences: list: pair describing the preference of the end-user
+    :return: list of strings: represents the execution trace chosen by the agent
+    """
+    goal_tree = build_tree(json_tree)
+    norm_tree = get_normed_tree(goal_tree, norm)
+    all_traces = get_all_valid_traces(norm_tree, beliefs, goal)
+
+    if len(all_traces) == 0:
+        return all_traces
+
+    # total cost of a trace is the sum of all costs of actions in the trace
+    nodes_by_name = {node.name: node for node in PreOrderIter(norm_tree)}
+    all_trace_costs = []
+    for trace in all_traces:
+        trace_costs = (
+            [nodes_by_name.get(t, None).costs for t in trace if hasattr(nodes_by_name.get(t, None), "costs")])
+        all_trace_costs.append([sum(col) for col in zip(*trace_costs)])
+
+    preferred_trace = all_trace_costs[0]
+    for t in all_trace_costs[1:]:
+        for i in preferences[1]:
+            if t[i] < preferred_trace[i]:
+                preferred_trace = t
+                break
+            elif t[i] > preferred_trace[i]:
+                break
+
+    return all_traces[all_trace_costs.index(preferred_trace)]
+
+
+# Exercise 3
+norm = {'type': 'O', 'actions': ['gotoShop', 'payShop', 'getCoffeeShop']}
+goal = ['haveCoffee']
+beliefs = ['staffCardAvailable', 'ownCard', 'colleagueAvailable', 'haveMoney', 'AnnInOffice']
+preferences = [['quality', 'price', 'time'], [2, 0, 1]]
+sol = ['getCoffee', 'getShopCoffee', 'gotoShop', 'payShop', 'getCoffeeShop']
+output = decision_making(json_tree, norm, goal, beliefs, preferences)
+print(output)
+print(output == sol)
--- Project_2/test_goal_tree.py
+++ Project_2/test_goal_tree.py
@@ -1,23 +1,72 @@
 import unittest
-from project2 import execution_traces, import_data_from_json
+from project2 import execution_traces, build_tree
+from assignment3 import decision_making
+import json
 
 
 class MyTestCase(unittest.TestCase):
-    coffee_tree = "coffee.json"
+    with open("coffee.json", "r") as file:
+        json_tree = json.load(file)
 
     def test_trace_getCoffeeKitchen(self):
-        trace_algorithm = execution_traces(self.coffee_tree, "getCoffeeKitchen")
+        trace_algorithm = execution_traces(build_tree(self.json_tree), "getCoffeeKitchen")
         sample_trace = [["getCoffeeKitchen"]]
         self.assertEqual(trace_algorithm, sample_trace)
 
     def test_trace_getCoffee(self):
-        trace_algorithm = execution_traces(self.coffee_tree, "getCoffee")
+        trace_algorithm = execution_traces(build_tree(self.json_tree), "getCoffee")
         sample_trace = [["getCoffee", "getKitchenCoffee", "getStaffCard", "getOwnCard", "gotoKitchen", "getCoffeeKitchen"],
                         ["getCoffee", "getKitchenCoffee", "getStaffCard", "getOthersCard", "gotoKitchen", "getCoffeeKitchen"],
                         ["getCoffee", "getAnnOfficeCoffee", "gotoAnnOffice", "getPod", "getCoffeeAnnOffice"],
                         ["getCoffee", "getShopCoffee", "gotoShop", "payShop", "getCoffeeShop"]]
 
         self.assertEqual(trace_algorithm, sample_trace)
+
+    def test_decision_haveCoffee(self):
+        norm = {'type': 'P', 'actions': ['gotoKitchen']}
+        goal = ['haveCoffee']
+        beliefs = ['haveMoney']
+        preferences = [['quality', 'price', 'time'], [2, 0, 1]]
+
+        sol = ['getCoffee', 'getShopCoffee', 'gotoShop', 'payShop', 'getCoffeeShop']
+        output = decision_making(self.json_tree, norm, goal, beliefs, preferences)
+
+        self.assertEqual(sol, output)
+
+    def test_decision_haveCoffee_2b(self):
+        norm = {'type': 'P', 'actions': ['payShop']}
+        goal = ['haveCoffee']
+        beliefs = ['staffCardAvailable', 'ownCard']
+        preferences = [['quality', 'price', 'time'], [1, 2, 0]]
+        sol = ['getCoffee', 'getKitchenCoffee', 'getStaffCard', 'getOwnCard', 'gotoKitchen', 'getCoffeeKitchen']
+
+        output = decision_making(self.json_tree, norm, goal, beliefs, preferences)
+
+        self.assertEqual(sol, output)
+
+    def test_decision_haveCoffee_haveMoney(self):
+        norm = {'type': 'P', 'actions': ['payShop']}
+        goal = ['haveCoffee']
+        beliefs = ['haveMoney']
+        preferences = [['quality', 'price', 'time'], [2, 0, 1]]
+        sol = []
+
+        output = decision_making(self.json_tree, norm, goal, beliefs, preferences)
+
+        self.assertEqual(sol, output)
+
+    def test_decision_haveCoffee_haveMoney(self):
+        norm = {'type': 'O', 'actions': ['gotoShop', 'payShop', 'getCoffeeShop']}
+        goal = ['haveCoffee']
+        beliefs = ['staffCardAvailable', 'ownCard', 'colleagueAvailable', 'haveMoney', 'AnnInOffice']
+        preferences = [['quality', 'price', 'time'], [2, 0, 1]]
+        sol = ['getCoffee', 'getShopCoffee', 'gotoShop', 'payShop', 'getCoffeeShop']
+
+        output = decision_making(self.json_tree, norm, goal, beliefs, preferences)
+
+        self.assertEqual(sol, output)
+
+
 
 
 if __name__ == '__main__':
