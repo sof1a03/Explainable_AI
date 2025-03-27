06b8e015b676642765ae2d5c5ac6873e8e40d5c0
Sofia Bianchini <167880833+sof1a03@users.noreply.github.com>
2025-03-27 12:44:43 +0100

    Update Assignment4.py

--- Project_2/Assignment4.py
+++ Project_2/Assignment4.py
@@ -17,8 +17,6 @@ assignment 3.
 with open("coffee1.json", "r") as file:
     json_tree = json.load(file)
 
-
-
 def build_tree(tree, parent=None):
     data = {
         key: (
@@ -88,7 +86,14 @@ def execution_traces(goal_tree_json, starting_node_name):
 
 
 def get_normed_tree(tree, norm):
+    def mark_no_violation(node):
+        node.violation = False
+        if hasattr(node, 'children'):
+            for child in node.children:
+                mark_no_violation(child)
+
     if len(norm) == 0:
+        mark_no_violation(tree)
         return tree
 
     def check_violation(node, norm):
@@ -275,6 +280,8 @@ def explain_action(json_tree, norm, goal, beliefs, preferences, action_to_explai
     u_factor = ["U", preferences]
     explanation = or_factors + p_factors + l_factors + d_factors + [u_factor]
     return selected_trace, explanation
+
+
 
 '''example 1'''
 # norm={'type': 'P', 'actions': ['gotoKitchen']}
